d0b02439e032aa51bd0d49b98438a21c8ab960da
xsh-matsuzaki <xsh-matsuzaki@kddi.com>
2017-08-14 18:04:37 +0900

    ステータスバーにアイコン追加

--- app/src/main/java/jp/shoma/screenfilter/ScreenFilterService.kt
+++ app/src/main/java/jp/shoma/screenfilter/ScreenFilterService.kt
@@ -1,5 +1,7 @@
 package jp.shoma.screenfilter
 
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
 import android.content.Intent
@@ -10,11 +12,13 @@ import android.widget.FrameLayout
 import android.graphics.PixelFormat
 import android.os.Build
 import android.support.annotation.RequiresApi
+import android.support.v4.app.NotificationCompat
 import android.view.View
 
 
 class ScreenFilterService : Service() {
-    private lateinit var view : View;
+    private lateinit var view : View
+
     override fun onBind(p0: Intent?): IBinder? {
         return null
     }
@@ -22,10 +26,23 @@ class ScreenFilterService : Service() {
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
+        val contentIntent = PendingIntent.getActivity(this, 0, Intent(this, MainActivity::class.java), 0)
+        val notification = NotificationCompat.Builder(this)
+                .setAutoCancel(false)
+                .setContentTitle("Screen Filter")
+                .setContentText("タップすると設定画面を起動できます")
+                .setContentIntent(contentIntent)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .build()
+        notification.flags = NotificationCompat.FLAG_ONGOING_EVENT
+
+        val nm = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        nm.notify(STATUS_BAR_ICON_ID, notification)
+
         val transparency = PrefUtil.getSpValInt(this, PrefConst.KEY_TRANSPARENCY)
         view = FrameLayout(this)
         view.setBackgroundColor(Color.BLACK)
-        view.background.alpha = 255 * transparency / 100
+        view.background.alpha = (255 * transparency / 100 * 0.8).toInt()
         val params = WindowManager.LayoutParams(
                 WindowManager.LayoutParams.MATCH_PARENT,
                 WindowManager.LayoutParams.MATCH_PARENT,
@@ -44,9 +61,12 @@ class ScreenFilterService : Service() {
         super.onDestroy()
         val wm = getSystemService(Context.WINDOW_SERVICE) as WindowManager
         wm.removeView(view)
+        val nm = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        nm.cancel(STATUS_BAR_ICON_ID)
     }
 
     companion object {
         val TAG = ScreenFilterService::class.java.name
+        val STATUS_BAR_ICON_ID = 0x01
     }
 }
